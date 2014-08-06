6e5c942678896c0f8c3f95b6a80e1c92a2dac91e
Jason Glose <glosej@rpi.edu>
2014-08-06 09:56:03 -0400

    Initial commit.

---
+++ BlogReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlogReader
+//
+//  Created by Jay Glose on 7/28/14.
+//  Copyright (c) 2014 Jay Glose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
